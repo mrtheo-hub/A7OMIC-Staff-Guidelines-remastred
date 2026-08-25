ecd6659219475e6fa6447ec21e1e0b431edf94b6
mrtheo-hub <mrtheoyt14@gmail.com>
2026-08-25 10:15:16 +0700

    Refactor logout handler to use Response.redirect

--- api/logout.js
+++ api/logout.js
@@ -1,11 +1,7 @@
 export const config = { runtime: 'edge' };
 
 export default async function handler(request) {
-  return new Response(null, {
-    status: 302,
-    headers: {
-      'Location': new URL('/login.html', request.url).toString(),
-      'Set-Cookie': 'staff_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0'
-    }
-  });
+  const res = Response.redirect(new URL('/login', request.url), 302);
+  res.headers.append('Set-Cookie', 'staff_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0');
+  return res;
 }
