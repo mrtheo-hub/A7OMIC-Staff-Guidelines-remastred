8b5b0b04ef1efab8b4095dcc01cfe704ab2a0e57
mrtheo-hub <mrtheoyt14@gmail.com>
2026-08-25 10:49:00 +0700

    Refactor logout handler to use Headers object

--- api/logout.js
+++ api/logout.js
@@ -1,7 +1,15 @@
 export const config = { runtime: 'edge' };
 
 export default async function handler(request) {
-  const res = Response.redirect(new URL('/login', request.url), 302);
-  res.headers.append('Set-Cookie', 'staff_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0');
-  return res;
+  const headers = new Headers();
+  headers.set('Location', new URL('/login', request.url).toString());
+  headers.append(
+    'Set-Cookie',
+    'staff_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0'
+  );
+
+  return new Response(null, {
+    status: 302,
+    headers,
+  });
 }
